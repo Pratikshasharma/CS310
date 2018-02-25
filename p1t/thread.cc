1c71335301eac8769ed5b308a6845127ff8cb9c9
pmk13 <pmk13@duke.edu>
2018-02-25 04:31:23 -0500

    first part nearly working!

--- p1t/thread.cc
+++ p1t/thread.cc
@@ -10,23 +10,20 @@
 
 using namespace std;
 
-struct thread_TCB //Individual Threads represented as a struct.
-{
-	ucontext_t *ucontext_ptr;
-	char *stack;
+ ucontext_t *ucontext_ptr;
+   
     
-};
 
 //DECLARE SHARED VARIABLES WITH STATIC
 /*
 
 Use this two to swap context
 */
-static thread_TCB *old_context;
-static ucontext *new_context;
+static ucontext_t *old_context;
 
-static std::deque<thread_TCB*> readyQueue ;
-static thread_TCB *curr_TCB;//The thread currently running.
+
+static std::deque<ucontext_t*> readyQueue ;
+static ucontext_t *curr=new ucontext_t;//The thread currently running.
 
 
 
@@ -47,30 +44,28 @@ extern int thread_libinit(thread_startfunc_t func, void *arg){
 	HAS_INIT=true;
 
 	//Initialize current thread/context to old_context.
-	old_context=new thread_TCB;
-	old_context->ucontext_ptr=new ucontext_t;
+    
+	old_context=new ucontext_t;
 
-	if (getcontext(old_context->ucontext_ptr)==-1)
+	if (getcontext(old_context)==-1)
 	{
     	//interrupt_enable();
     	return -1;
 	}
     
 	//old_context->DONE=false;
-	old_context->stack=new char[STACK_SIZE];
-	old_context->ucontext_ptr->uc_stack.ss_sp=old_context->stack;
-	old_context->ucontext_ptr->uc_stack.ss_size=STACK_SIZE;
-	old_context->ucontext_ptr->uc_stack.ss_flags=0;
-	old_context->ucontext_ptr->uc_link=NULL;
+	char *stack=new char[STACK_SIZE];
+	old_context->uc_stack.ss_sp=stack;
+	old_context->uc_stack.ss_size=STACK_SIZE;
+	old_context->uc_stack.ss_flags=0;
+	old_context->uc_link=NULL;
 
 	//Declare Parent thread:
-	thread_TCB *parent;
-	parent = new thread_TCB;// Use new to avoid copying ucontext struct
-	parent->ucontext_ptr=new ucontext_t;
-
+	ucontext_t *parent=new ucontext_t;
+   
 
 	//Parent thread initialization
-	int err_get = getcontext(parent->ucontext_ptr);
+	int err_get = getcontext(parent);
 	if (err_get==-1)
 	{
     	//interrupt_enable();
@@ -78,95 +73,98 @@ extern int thread_libinit(thread_startfunc_t func, void *arg){
 	}
 
 	//parent->DONE=false;
-	parent->stack=new char[STACK_SIZE];
-	parent->ucontext_ptr->uc_stack.ss_sp=parent->stack;
-	parent->ucontext_ptr->uc_stack.ss_size=STACK_SIZE;
-	parent->ucontext_ptr->uc_stack.ss_flags=0;
-	parent->ucontext_ptr->uc_link=NULL;
+	char *p_stack=new char[STACK_SIZE];
+	parent->uc_stack.ss_sp=p_stack;
+	parent->uc_stack.ss_size=STACK_SIZE;
+	parent->uc_stack.ss_flags=0;
+	parent->uc_link=NULL;
 
 	//modify context pointer
-	makecontext(parent->ucontext_ptr, (void (*)()) start, 2, func, arg);
+	makecontext(parent, (void (*)()) start, 2, func, arg);
 
-	curr_TCB=parent;
-	readyQueue.push_back(parent);//PUSH TO BACK OF READY QUEUE.
+	curr=old_context;
 
-	if (swapcontext(old_context->ucontext_ptr, parent->ucontext_ptr)==-1)
+	if (swapcontext(old_context, parent)==-1)
 	{
    	// interrupt_enable();
     	return -1;
 	}
 
 	//DELETE THREADS IF OLD_CONTEXT IS EVER REACTIVATED
-	
+    
 	// TODO: CLEAN UP AND FREE SPACE.
-	delete curr_TCB->stack;
-	curr_TCB->ucontext_ptr->uc_stack.ss_sp=NULL;
-	curr_TCB->ucontext_ptr->uc_stack.ss_size=0;
-	curr_TCB->ucontext_ptr->uc_stack.ss_flags=0;
-	delete curr_TCB->ucontext_ptr;
-	delete curr_TCB;
+    
+   while(!(readyQueue.empty())){
+    
+	delete curr->uc_stack.ss_sp;
+	curr->uc_stack.ss_sp=NULL;
+	curr->uc_stack.ss_size=0;
+	curr->uc_stack.ss_flags=0;
+	delete curr;
+    
+	curr=readyQueue.front();
+	readyQueue.pop_front();
+	cout << "I did reached cleanup ON WHILE LOOP END\n";
 
-	if (readyQueue.empty())
-
+    
+	//TODO:Switch to another thread
+   if (swapcontext(old_context,curr)==-1)
 	{
-
-		cout << "Thread library exiting.\n";
-		exit(0);
-
+    
+    	//interrupt_enable();
+    	return -1;
 	}
-	//Switch to another thread
-	curr_TCB=readyQueue.front();
+    
 
-	swapcontext(old_context->ucontext_ptr,curr_TCB->ucontext_ptr);
+}
 
+    
+    	cout << "Thread library exiting.\n";
+    	exit(0);
 
-
-//interrupt_enable();
-return 0;
+    
 
 }
 
 extern int thread_create(thread_startfunc_t func, void *arg){
-    //interrupt_disable();
+	//interrupt_disable();
 	if (HAS_INIT==false)
 
 	{
     	//interrupt_enable();
+    
     	return -1;
 	}
+    
 	//Declare child thread:
-	thread_TCB *child;
-	child = new thread_TCB;// Use new to avoid copying ucontext struct
-	child->ucontext_ptr=new ucontext_t;
-
-
+	ucontext_t *child=new ucontext_t;
+    
 	//child thread initialization from curr context
-	int err_get = getcontext(child->ucontext_ptr);
+	int err_get = getcontext(child);
 	if (err_get==-1)
 	{
+    
     	//interrupt_enable();
     	return -1;
 	}
 
 	//child->DONE=false;
-	child->stack=new char[STACK_SIZE];
-	child->ucontext_ptr->uc_stack.ss_sp=child->stack;
-	child->ucontext_ptr->uc_stack.ss_size=STACK_SIZE;
-	child->ucontext_ptr->uc_stack.ss_flags=0;
-	child->ucontext_ptr->uc_link=NULL;
+	char *stack=new char[STACK_SIZE];
+	child->uc_stack.ss_sp=stack;
+	child->uc_stack.ss_size=STACK_SIZE;
+	child->uc_stack.ss_flags=0;
+	child->uc_link=NULL;
 
 
 
-	makecontext(child->ucontext_ptr, (void (*)()) start, 2, func, arg);
+	makecontext(child, (void (*)()) start, 2, func, arg);
     
 
-	//curr_TCB=child;
 	readyQueue.push_back(child);//PUSH TO BACK OF READY QUEUE.
-	//swapcontext(old_context->ucontext_ptr,child->ucontext_ptr);
 	//interrupt_enable();
 
 
-	exit(0);
+	return 0;
 
 }
 
@@ -177,22 +175,42 @@ extern int thread_yield(void){
     	//interrupt_enable();
     	return -1;
 	}
-	if (readyQueue.empty())
-	{
-   	// interrupt_enable();
-    	return 0;
-	}
+    
+    
 
-	readyQueue.push_back(curr_TCB);
-	thread_TCB *new_one=readyQueue.front();
-	readyQueue.pop_front();
+    //cout <<curr<<'\n';
+    //cout <<readyQueue.front()<<'\n';
 
+    
+    if(readyQueue.empty()){
+    return 0;
+
+}
+   
+    
+    ucontext_t *oldOne;
+    oldOne=curr;
+
+    curr=readyQueue.front();
+    readyQueue.pop_front();
+    
+   	 readyQueue.push_back(oldOne);
+    //cout << "Size is"<<readyQueue.size()<<'\n';
+    
+    
 	//TODO:
-	if (swapcontext(curr_TCB->ucontext_ptr,new_one->ucontext_ptr)==-1)
+	if (swapcontext(oldOne,curr)==-1)
 	{
     	//interrupt_enable();
+    
     	return -1;
 	}
+   	// interrupt_enable();
+  	 
+    
+    
+    
+    
 	 
  	//interrupt_enable();
  	return 0;
@@ -200,6 +218,7 @@ extern int thread_yield(void){
 }
 //Stub Function
 int start(thread_startfunc_t func,void *arg){
+    
 
    // interrupt_disable();
 	if (HAS_INIT==false)
@@ -208,24 +227,25 @@ int start(thread_startfunc_t func,void *arg){
     	return -1;
 	}
 
+	 
     
-	//interrupt_enable();
-    curr_TCB=readyQueue.front();
-	readyQueue.pop_front();//remove from ready since it now enters run mode.
-	func(arg);//Thread has run!
-   // interrupt_disable();
+	func(arg);
 
-    //DELETE THREAD AFTER IT FINISHES RUNNING:WE HAVE TO GO TO ANOTHER CONTEXT TO DO 
-    //Go back to old context. Seems to be only context not modified.
-    if (swapcontext(curr_TCB->ucontext_ptr,old_context->ucontext_ptr)==-1)
+	//DELETE THREAD AFTER IT FINISHES RUNNING:WE HAVE TO GO TO ANOTHER CONTEXT TO DO SO.
+	//Go back to old context. Seems to be only context not modified.
+	if (swapcontext(curr,old_context)==-1)
 	{
     	//interrupt_enable();
+    
     	return -1;
 	}
 
   //interrupt_enable();
+    return 0;//Thread exits
 
 }
+
+
 
 
 
