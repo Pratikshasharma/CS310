623694bb65cd5020456c3ad53e6ed70e33c056ca
ima10 <ima10@duke.edu>
2018-02-27 04:27:24 -0500

    Updated Test4.cc

--- p1t/test4.cc
+++ p1t/test4.cc
@@ -1,16 +1,20 @@
-
 #include "thread.h"
 #include "interrupt.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
-
+#include <fstream>
+#include <cmath>
+#include<vector>
+#include <deque>
 
 using namespace std;
 
 // Test if a thread unlocks the lock it does not have
 // and gets the lock it has
+
+/*RETURNS ERROR - EDIT HERE */
 
 unsigned int lock1=5;
 unsigned int lock2=10;
@@ -19,28 +23,34 @@ int createThreads();
 int thread2();
 
 
-
-int main(){
-	// thread_create(createThreads,NULL);
-	thread_create((thread_startfunc_t)createThreads,(void*) 0);
-	exit(0);
+int main() {
+  if (thread_libinit( (thread_startfunc_t) createThreads, (void *) 100)) {
+    cout << "thread_libinit failed\n";
+    exit(1);
+  }
 }
 
 int createThreads(){
-	thread_lock(lock1);
-	std::cout << thread_lock(lock1) << endl;
-	std::cout << "Locked lock1" << endl;
-	// thread_create((thread_startfunc_t) thread2,(void*) 0);
-	thread_create( thread2,(void*) 0);
+	thread_lock(lock1); 
+	//cout <<thread_lock(lock1)<<endl; //This would also include a test for trying to lock() a lock you already have
+	cout<<"Locked lock1" << endl;
+
+  	if (thread_create((thread_startfunc_t) thread2, (void *) "thread 2")) {
+  	  cout << "thread_create failed\n";
+  	  exit(1);
+  	}
+
 	thread_yield();
-	thread_unlock(lock2);
-	std::cout << "Should not unlock lock2" << endl;
+	if(thread_unlock(lock2)==-1){
+	cout << "Did not unlock lock2" << endl;	
+	}
+	cout<< "Should not unlock lock2" << endl;
 	thread_unlock(lock1);
 }
 
 int thread2(){
 	thread_lock(lock2);
-	std::cout << "thread 2 runs with lock2 " << endl;
+	cout<<" thread 2 runs with lock2 " <<endl;
 	thread_yield();
 	thread_unlock(lock2);
 }
