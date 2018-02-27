1b67dded09e75f1724f5ba71c90acb94fd5822b3
ima10 <ima10@duke.edu>
2018-02-26 20:12:43 -0500

    Update thread.cc

--- p1t/thread.cc
+++ p1t/thread.cc
@@ -1,516 +1,297 @@
 #include "thread.h"
 #include "interrupt.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <ucontext.h>
-#include <deque>
-//#include <unordered_map>
 #include <iostream>
-#include <map>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <deque>
 
 using namespace std;
 
-ucontext_t *ucontext_ptr;
-   
-    
-//DECLARE SHARED VARIABLES WITH STATIC
-/*
 
-Use this two to swap context
-*/
-
-static ucontext_t *old_context;
+int maxOrders; // maxOrders on the board 
+int NUM_CASHIERS; // total initial number of cashiers 
+int recentOrder; // recently completed order by the maker
+int currentOrders=0; // current number of orders in the board
+int aliveCashiers=0; // number of alive cashiers 
 
 
-static std::deque<ucontext_t*> readyQueue;
-static ucontext_t *curr=new ucontext_t;//The thread currently running.
-
-static bool HAS_INIT=false;// Boolean to make sure we don't initialize thread library twice
-
-
-int start(thread_startfunc_t func, void *arg); //The stub Function as described in lecture!
+/* locks */
+unsigned int SANDWICHMAKER_LOCK= 100;
+unsigned int CASHIER_LOCK = 200;
+int CV_BOARD_NOT_FULL = 300;
+int CV_BOARD_FULL = 400;
 
 
-static map< unsigned int, std::deque<ucontext_t*> > allLocks; //A Hashmap of each lockID to lock_queue. The first item of the lock_queue is the lock that has aquired the ucontext_t. 
-
-static map<std::pair<unsigned int, unsigned int>, std::deque<ucontext_t*> > monitors; //A Hashmap of each Monitor / mutex to the cv_queue. 
 
 
-extern int thread_libinit(thread_startfunc_t func, void *arg){
-	interrupt_disable();
-	if (HAS_INIT==true)
-	{
-   	 interrupt_enable();
-    	return -1;
+struct order {
+	int cashier;
+	int sandwich;
+};
+
+std::vector<order> ordersOnBoard;
+std::vector<int> leftSandwichOrders; /* store the sandwich orders left for each cashier */
+std::vector<std::deque<int> >totalSandwichList;
+
+
+void cashier(void* args);
+void sandwichMaker(void* args);
+void createThreads(void* args);
+bool isCashierOnBoard(int cashierNum);
+
+// DEBUG
+void printOrdersOnBoard();
+
+
+
+char** ORDER_FILES;
+
+
+
+// When the fewer than the no of cahsiers- then thats the max ordersw
+
+	void cashier(void* args){
+		/* lock the thread first */
+		// printf(" cashier \n");
+
+		// thread_lock(CASHIER_LOCK);
+
+		int cashierNum = (long int) args;
+		std::deque<int> sandwiches = totalSandwichList[cashierNum];
+
+		// printf(" cashier %d\n", cashierNum);
+		// printf("Sandwich Size  : %d \n", sandwiches.size());
+
+		/* Check if the cashier is alive */
+		while(sandwiches.size()>0  && !isCashierOnBoard(cashierNum)){
+			/* lock the thread */
+			thread_lock(CASHIER_LOCK);
+
+			/*  if the board is full */
+			while(currentOrders >= maxOrders){
+				// wait for board to free space
+				thread_wait(CASHIER_LOCK,CV_BOARD_NOT_FULL);
+			}
+
+
+			// push order into the board
+			// if(sandwiches.size() >0){
+				// create an order
+				order myOrder;
+				myOrder.sandwich = sandwiches.front();
+				myOrder.cashier = cashierNum;
+
+				// Empty the sandwiches vector
+				sandwiches.pop_front();
+
+				/* add remaining sandwich Orders to the vector */
+
+				// printf("sandwich size after: %d \n ", sandwiches.size());
+
+				// add order on boarao-d 
+				ordersOnBoard.push_back(myOrder);
+				// printf("board size after: %d \n ", ordersOnBoard.size());
+				
+				currentOrders++;
+				// printf("Current orders: %d \n ", currentOrders);
+
+				cout << "POSTED: cashier " << cashierNum << " sandwich " << myOrder.sandwich << endl;
+
+				// signal new order
+				if(currentOrders == maxOrders){
+					// printf(" SIGNAL BOARD FULL \n");
+					thread_signal(SANDWICHMAKER_LOCK,CV_BOARD_FULL);
+				}
+				// unlock the sandwichmaker thread
+				// thread_unlock(SANDWICHMAKER_LOCK);
+				// Wait for order to be completed
+
+				// printf(" SHOULD BE HERE \n");
+				// wait for your sandwich to be made 
+
+				// printf("Waiting before  %d\n", myOrder.sandwich);
+				thread_wait(CASHIER_LOCK,myOrder.sandwich);
+
+				// printf("Waiting after  %d\n", myOrder.sandwich);
+
+				cout << "READY: cashier " << cashierNum << " sandwich " << myOrder.sandwich << endl;
+
+				if(sandwiches.size()==0){
+					aliveCashiers--;
+				}
+
+
+
+				// wait for signal to post next order -- probably not needed ?? 
+				// thread_wait(CASHIER_LOCK,CV_BOARD_NOT_FULL); /* To release the lock */
+
+				// printf(" AFTER WAKE UP %d \n ", cashierNum);
+			// }
+		}
+		// printf(" sandwichSize %d , %d\n",sandwiches.size(),cashierNum);
+		// printf("alive Cashiers Before %d\n",aliveCashiers);
+		// aliveCashiers--;
+		// printf("alive Cashiers %d\n", aliveCashiers);
+		// signal maker order has been posted
+		if(aliveCashiers < maxOrders && aliveCashiers > 0){
+			// adjust maxOrders
+			maxOrders--;
+			thread_signal(SANDWICHMAKER_LOCK,CV_BOARD_FULL);
+		}
+		// unlock thread from cashiser
+		thread_unlock(CASHIER_LOCK);
+
+		// printf(" maxOrders %d\n", maxOrders);
+		// printf(" aliveCashiers %d\n", aliveCashiers);
+		return;
+	}
+
+
+	void sandwichMaker(void* args){
+		// printf(" maker \n ");
+		
+		int lastSandwich = -1;
+
+		// check if board has an order
+		while(aliveCashiers > 0 ){
+			
+			thread_lock(SANDWICHMAKER_LOCK);
+			// Wait for order to be posted if board is not full
+			while(ordersOnBoard.size() < maxOrders){
+				// signal the thread to post
+				thread_signal(CASHIER_LOCK,CV_BOARD_NOT_FULL);
+
+				thread_wait(SANDWICHMAKER_LOCK,CV_BOARD_FULL);
+				// printf("NOT MAX ORDERS %d, %d, ", ordersOnBoard.size(), maxOrders);
+			}
+			// printf(" alive cashiers maker %d \n ", aliveCashiers);
+
+			// unlock if no more alive cashiers
+			if(aliveCashiers ==0){
+				thread_unlock(SANDWICHMAKER_LOCK);
+			}
+
+			// get the index of next sandwich 
+			int minimumDiff = 999;
+			int index;
+			for(int i=0; i < ordersOnBoard.size();i++){
+				order myOrder = ordersOnBoard[i];
+				if(std::abs(myOrder.sandwich - lastSandwich) < minimumDiff){
+					minimumDiff = std::abs(myOrder.sandwich - lastSandwich);
+					index = i;
+				}
+			}
+
+			// update lastSandwich
+			lastSandwich = ordersOnBoard[index].sandwich;
+			
+			// printf(" BEFORE \n");
+			// printOrdersOnBoard();
+
+			// reduce currentOrders.no-
+			currentOrders--;
+			// remove from orders vector
+			ordersOnBoard.erase(ordersOnBoard.begin() + index);
+
+			// printf(" AFTER \n");
+
+			// printOrdersOnBoard();
+
+			// signal the cashier
+			thread_signal(CASHIER_LOCK,lastSandwich);
+
+			// thread_wait(SANDWICHMAKER_LOCK,CV_BOARD_FULL); 
+			// broadcast order can be posted
+			thread_broadcast(CASHIER_LOCK,CV_BOARD_NOT_FULL);
+
+		}
+		// 
+		thread_unlock(SANDWICHMAKER_LOCK);
+		return;
 
 	}
-	HAS_INIT=true;
 
-	//Initialize current thread/context to old_context.
-    
-	old_context=new ucontext_t;
 
-	if (getcontext(old_context)==-1)
-	{
-    	interrupt_enable();
-    	return -1;
-	}
-    
-	//old_context->DONE=false;
-	char *stack=new char[STACK_SIZE];
-	old_context->uc_stack.ss_sp=stack;
-	old_context->uc_stack.ss_size=STACK_SIZE;
-	old_context->uc_stack.ss_flags=0;
-	old_context->uc_link=NULL;
-
-	//Declare Parent thread:
-	ucontext_t *parent=new ucontext_t;
-   
-
-	//Parent thread initialization
-	int err_get = getcontext(parent);
-	if (err_get==-1)
-	{
-    	interrupt_enable();
-    	return -1;
+	bool isCashierOnBoard(int cashierNum){
+		for(int i =0; i < ordersOnBoard.size();i++){
+			if(ordersOnBoard[i].cashier == cashierNum){
+				return true;
+			}
+		}
+		return false;
 	}
 
-	//parent->DONE=false;
-	char *p_stack=new char[STACK_SIZE];
-	parent->uc_stack.ss_sp=p_stack;
-	parent->uc_stack.ss_size=STACK_SIZE;
-	parent->uc_stack.ss_flags=0;
-	parent->uc_link=NULL;
+	int main (int argc, char* argv[]){
+		NUM_CASHIERS = argc - 2;
 
-	//modify context pointer
-	makecontext(parent, (void (*)()) start, 2, func, arg);
+		if(NUM_CASHIERS < atoi(argv[1])){
+			maxOrders = NUM_CASHIERS;
+		}else{
+			maxOrders = atoi(argv[1]);
+		}
+		aliveCashiers = NUM_CASHIERS;
+		// ORDER_FILES = argv;
+		ORDER_FILES = (char **) ((void*)argv);
+		
+		// cout << "The current files are:" << '\n ';
+	 	//  	for(int i = 0; i < NUM_CASHIERS;i++){
+	 	//  		std::cout << ' ' << ORDER_FILES[i+2];
+	 	//  		cout << '\n';
+	 	//  	}
 
-	curr=parent;
+	  	/* create threads */
+	  	thread_libinit( (thread_startfunc_t) createThreads,(void*)0);
 
-	if (swapcontext(old_context, parent)==-1)
-	{
-   	 interrupt_enable();
-    	return -1;
 	}
 
-	//DELETE THREADS IF OLD_CONTEXT IS EVER REACTIVATED
-    
-	// TODO: CLEAN UP AND FREE SPACE.
-interrupt_enable();
-    
-   while(!(readyQueue.empty())){
-    
-	delete curr->uc_stack.ss_sp;
-	curr->uc_stack.ss_sp=NULL;
-	curr->uc_stack.ss_size=0;
-	curr->uc_stack.ss_flags=0;
-	delete curr;
-    
-	curr=readyQueue.front();
-	readyQueue.pop_front();
-	cout << "I did reached cleanup ON WHILE LOOP END\n";
 
-    interrupt_disable();
-	//TODO:Switch to another thread
-   if (swapcontext(old_context,curr)==-1)
-	{
-    
-    	interrupt_enable();
-    	return -1;
+	/* creates threads for the sandwich maker and the cashiers */
+	void createThreads(void* args){
+		// printf(" CREATE THREADS \n ");
+
+		/* create sandwich maker thread */
+		thread_create((thread_startfunc_t) sandwichMaker, (void*) 0 );
+
+		char str[255];
+
+		for(int i=0; i < NUM_CASHIERS;i++){
+			char* orderFile = (char*) ((void*)ORDER_FILES[i+2]);
+			ifstream fileStream(orderFile);
+
+			std:: deque<int> sandwiches ;
+			while(!fileStream.eof()){
+				fileStream.getline(str,255);
+				if(!fileStream.eof() && !str[0]==0){
+					int order_number = atoi(str);
+					sandwiches.push_back(order_number);
+					// cout << "Order: " << order_number << endl;
+				}
+
+			}
+
+			/* Add sandwiches to the list */
+			totalSandwichList.push_back(sandwiches);
+
+			/* Empty the vector */
+			while(sandwiches.empty()){
+				sandwiches.pop_back();
+			}
+
+			/* create thread for each cashier */
+			thread_create((thread_startfunc_t) cashier,(void*)(long)i);
+		}
+
 	}
-interrupt_enable();
-    
 
+
+/* Debugging - print the orders on board */
+void printOrdersOnBoard(){
+	for(int i =0; i < ordersOnBoard.size();i++){
+		order myOrder = ordersOnBoard[i];
+		printf("Cashier: %d , Sandwich: %d \n", myOrder.cashier,myOrder.sandwich);
+	}
 }
-
-    	//interrupt_enable();
-    	cout << "Thread library exiting.\n";
-    	exit(0);
-
-    
-
-}
-
-extern int thread_create(thread_startfunc_t func, void *arg){
-	interrupt_disable();
-	if (HAS_INIT==false)
-	{
-    	interrupt_enable();
-    
-    	return -1;
-	}
-    
-	//Declare child thread:
-	ucontext_t *child = new ucontext_t;
-    
-	//child thread initialization from curr context
-	int err_get = getcontext(child);
-	if (err_get==-1)
-	{ 
-    	interrupt_enable();
-    	return -1;
-	}
-
-	
-	char *stack=new char[STACK_SIZE];
-	child->uc_stack.ss_sp=stack;
-	child->uc_stack.ss_size=STACK_SIZE;
-	child->uc_stack.ss_flags=0;
-	child->uc_link=NULL;
-
-
-
-	makecontext(child, (void (*)()) start, 2, func, arg);
-    
-
-	readyQueue.push_back(child);//PUSH TO BACK OF READY QUEUE.
-	interrupt_enable();
-
-
-	return 0;
-
-}
-
-extern int thread_yield(void){
-    interrupt_disable();
-    if (HAS_INIT==false)
-    {
-        interrupt_enable();
-    	return -1;
-    }
-    
-
-    //cout <<curr<<'\n';
-    //cout <<readyQueue.front()<<'\n';
-
-    
-    if(readyQueue.empty()){
-	interrupt_enable();
-       return 0;
-    }
-   
-    
-    ucontext_t *oldOne;
-    oldOne=curr;
-
-    curr=readyQueue.front();
-    readyQueue.pop_front();
-    
-    readyQueue.push_back(oldOne);
-    //cout << "Size is"<<readyQueue.size()<<'\n';
-    
-    
-	//TODO:
-    if (swapcontext(oldOne,curr)==-1)
-	{
-    	interrupt_enable();
-    
-    	return -1;
-	}
-   	 //interrupt_enable();
-  	 
-
- 	interrupt_enable();
-    return 0;
-
-}
-//Stub Function
-int start(thread_startfunc_t func,void *arg){
-    
-
-   
-	if (HAS_INIT==false)
-	{
-    	interrupt_enable();
-    	return -1;
-	}
-
-    	interrupt_enable();
-	func(arg);
-	interrupt_disable();
-
-	//DELETE THREAD AFTER IT FINISHES RUNNING:WE HAVE TO GO TO ANOTHER CONTEXT TO DO SO.
-	//Go back to old context. Seems to be only context not modified.
-	if (swapcontext(curr,old_context)==-1)
-	{
-    	interrupt_enable();
-    
-    	return -1;
-	}
-
-  interrupt_enable();
-    return 0;//Thread exits
-
-}
-
-
-//static map<std::pair<unsigned int, unsigned int>, std::deque<thread_TCB*> > monitors;
-int thread_wait(unsigned int lockID, unsigned int cvID){
-    
-
-   //IMPORTANT / TO ADD - CALL UNLOCK(lockID)!! (I think we might need a stub instead of something else?)
-   //Error: The thread tries to unlock a lock it doesn't have:
-	interrupt_disable();
-
-	if (HAS_INIT==false)
-	{
-    	interrupt_enable();
-    	return -1;
-	}
-  if( allLocks.find(lockID) == allLocks.end() ){ //If the lock isn't in the map:
-	interrupt_enable();
-  	return 0;
-  }
-
-  if( allLocks[lockID].front()!=curr ) // The front of lockID's queue will be the TCB that currently holds the lock. 
-   {
-	interrupt_enable();
-         return 0; //error
-   }
-   std::pair<unsigned int, unsigned int> new_pair (lockID, cvID);
-
-   if( monitors.find(new_pair) == monitors.end() ) // if pair / lockID-cvID is not found
-   {
-       std::deque<ucontext_t*> new_queue; 
-       monitors.insert( std::make_pair(new_pair, new_queue) ); //then make the pair
-   }	
-
-   std::deque<ucontext_t*>& sleep_queue = monitors[new_pair]; // get queue associated w/pair
-   
-   /* Perform switch stuff: */
-   ucontext_t* old_thread; // store the current thread. *** note is it okay that old_TCB is local?***
-   old_thread = curr;
-	interrupt_enable();
-    thread_unlock(lockID);
-	interrupt_disable();
-	  
-   if(readyQueue.empty())
-   {
-	  cout << "Thread library exiting.\n"; 
-	  exit(0);// can't pop from an empty Q --or should it be 0?
-   }
-  
-
-   curr = readyQueue.front(); //changes curr -> to front of ready list
-   readyQueue.pop_front();
-   
-   sleep_queue.push_back(old_thread);//PUSHES RUNNING THREAD TO BACK OF PAIR"S SLEEP_QUEUE
-  
-
-   swapcontext(old_thread, curr); 
-	interrupt_enable();
-	thread_lock(lockID);
-	interrupt_disable();
-   
-   /* We will reactivate the waiting thread : */ 
-interrupt_enable(); 
-
-   return 0;
-}
-
-
-
-int thread_signal(unsigned int lockID, unsigned int cvID){
-   // Put head of cvID's sleep_queue on the tail of the ready queue BLOCKED -> READY
-	// if not in map of locks, create it:
-	interrupt_disable();
-
-	if (HAS_INIT==false)
-	{
-    	interrupt_enable();
-    	return -1;
-	}
-    if (allLocks.find(lockID) == allLocks.end())
-
-        {
-	      interrupt_enable();
-              return 0;
-	}
-
-   std::pair<unsigned int, unsigned int> new_pair (lockID, cvID);
-
-   if( monitors.find(new_pair) == monitors.end() ) // if pair / lockID-cvID is not found
-        {
-	      interrupt_enable();
-              return 0;
-	}
-
-   std::deque<ucontext_t*>& sleep_queue = monitors[new_pair]; 
-   
-   ucontext_t* new_ready = sleep_queue.front(); 
-   sleep_queue.pop_front();
-	
-   readyQueue.push_back(new_ready); // push front of sleep queue to ready 
-	interrupt_enable();
-   
-   return 0;
-}
-
-int thread_broadcast(unsigned int lockID, unsigned int cvID){
-	interrupt_disable();
-
-	if (HAS_INIT==false)
-	{
-    	interrupt_enable();
-    	return -1;
-	}
-   // Put all of the cvID's sleep_queue on the ready queue. BLOCKED -> READY
-    if (allLocks.find(lockID) == allLocks.end())
-        {
-	      interrupt_enable();
-              return 0;
-	}
-
-   std::pair<unsigned int, unsigned int> new_pair (lockID, cvID);
-
-   if( monitors.find(new_pair) == monitors.end() ) // if pair / lockID-cvID is not found
-        {
-	      interrupt_enable();
-              return 0;
-	}
-
-   std::deque<ucontext_t*>& sleep_queue = monitors[new_pair]; 
-
-   while(!sleep_queue.empty()){
-	 ucontext_t* new_ready = sleep_queue.front(); 
-         sleep_queue.pop_front();
-	
-         readyQueue.push_back(new_ready); 
-
-   }
-	interrupt_enable();
-   return 0;
-}
-
-
-
-
-/*Input is lockID*/
-
-//static std::unordered_map<unsigned int, std::deque<thread_TCB*> > allLocks; <---note this is above
-int thread_lock(unsigned int lockID){
-    interrupt_disable(); 
-
-	if (HAS_INIT==false)
-	{
-    	interrupt_enable();
-    	return -1;
-	}
-    
-
-    // if not in map of locks, create it:
-    if (allLocks.find(lockID) == allLocks.end())
-        {
-              std::deque<ucontext_t*> new_queue;
-              allLocks.insert(std::make_pair(lockID, new_queue)); //***SYNTAX CHECK
-	}
-
-    // Now it is in the map
-    std::deque<ucontext_t*>& lock_queue = allLocks[lockID];
-    if( !(lock_queue.empty()) )
-    {
-       if( lock_queue.front() == curr ) //Error: Thread tries to aquire a lock it already has
-       {
-	     interrupt_enable();
-	     return -1; 
-       }
-    }
-    if ( lock_queue.empty() ) //if(Lock == FREE): curr aquires the lock!
-       {
-              lock_queue.push_front(curr); // put curr at the front of lock_queue, curr aquired the lock!
-	      interrupt_enable();
-	      return 0;
-       }
-	
-     //Elif the lock is busy then current thread is pushed to the lock_queue. We perform a swap context. 
-     else
-        { 
-          ucontext_t* old_thread; // store the current thread. *** note is it okay that this is local?***
-          old_thread = curr;
-	  
-          if(readyQueue.empty()) //person aquired lock, can't be on ready, can't be on lock, can't be running -> must be waiting.
-	  {
-		//interrupt_enable();
-	       cout << "Thread library exiting.\n"; // We think this is a deadlock ! 
-    	       exit(0); 
-          }
-
-          curr = readyQueue.front(); //changes curr to front of ready list
-          readyQueue.pop_front();
-
-	  lock_queue.push_back(old_thread);//PUSHES RUNNING THREAD TO BACK OF LOCK QUEUE
-
- 	  swapcontext(old_thread, curr);  //RUNS FRONT OF READY QUEUE
-	 interrupt_enable();
-
-	}
-
-   
-    return 0;
-}
-
-
-//Input is lockID - Somewhere need to make a stub so wait() can call unlock
-int thread_unlock(unsigned int lockID){
-    interrupt_disable();
-
-	if (HAS_INIT==false)
-	{
-    	interrupt_enable();
-    	return -1;
-	}
-
-    if (allLocks.find(lockID) != allLocks.end()) //If the lock is in the map:
-    {
-        //Error: The thread tries to unlock a lock it doesn't have:
-        if( !(allLocks[lockID].empty()) ) 
-        {
-	  if(allLocks[lockID].front() != curr ) 
-	   {
-		interrupt_enable();
-	        return -1;
-	   }
-	}
-
-        else // if the queue is empty: (error!)
-        {
-	    interrupt_enable();
-	    return -1;
-	}
-
-        /*
-        if( allLocks[lockID].front() != curr or allLocks[lockID].empty() )
-        {
-          return -1; //error
-        }*/
-
-        std::deque<ucontext_t*>& lock_queue = allLocks[lockID]; // Get lockID from the map. 
-	
-        allLocks[lockID].pop_front(); // Current thread releases the lock! But keeps running. (aquired lockID == ucontext at front of it's lock_queue)
-	
-        if(!(lock_queue.empty()) ) // IF a thread is waiting on the lock queue: BLOCKED/lQ -> READY QUEUE & AQUIRES LOCK
-        {
-        
-        ucontext_t* t = lock_queue.front(); // Store thread_TCB from the top of lock queue 
-        //lock_queue.pop_front(); //nvm DON'T pop this TCB, keep at top of lock queue as t aquired the lock! 
-        // Leave lock as aquired by this thread! (Hand off locks from lecture)
-
-        readyQueue.push_back(t); // Put t from lockqueue on back of ready queue  BLOCKED -> READY QUEUE
-
-        }
-    }
-
-    // else lockID not in the map, than no lock to unlock-> user error return -1.
-    else
-    {	interrupt_enable();
-        return 0;
-    }
-
-    interrupt_enable();
-    return 0;
-}
-
-
-
-
