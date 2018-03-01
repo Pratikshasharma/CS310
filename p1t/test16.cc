067c0df1d2b83652dd105da3ea61fa6c9b4f3e7f
PRATIKSHA <ps179@duke.edu>
2018-03-01 01:11:29 -0500

    Added test 16 and 17

--- p1t/test16.cc
+++ p1t/test16.cc
@@ -10,6 +10,11 @@ using namespace  std;
 
 // Test a thread yielding to itself 
 void createThreads(void* args);
+void createChildThreads(void* args);
+unsigned int lock = 2;
+
+int global = 0;
+
 
 
 int main() {
@@ -21,11 +26,33 @@ int main() {
 
 
 void createThreads(void* args){
-	cout << thread_lock(1) << endl;
+	cout << thread_lock(lock) << endl;
+	cout << thread_create((thread_startfunc_t) createChildThreads,(void*)1) << endl;
+
+	cout << thread_create((thread_startfunc_t) createChildThreads,(void*)2) << endl;
+	
+	
+	cout << thread_broadcast(lock,1) << endl;
+
+	cout << thread_unlock(lock) << endl;
+
 	cout << thread_yield() << endl;
-	// cout << thread_unlock(1) << endl;
-	if(thread_unlock(1)==-1){
-		cout << "thread yielding to itself failed" << endl;
+
+}
+
+void createChildThreads(void* args){
+	cout << "child thread " << endl;
+	cout << ((long) args)<< endl;
+	
+	thread_lock(lock);
+	global = global + (long) args ;
+	cout << global << endl;
+
+	while (global >= 3){
+		thread_wait(lock,1);
 	}
+	
+	cout << global << endl;
+	cout << thread_unlock(lock) << endl;
 }
 
